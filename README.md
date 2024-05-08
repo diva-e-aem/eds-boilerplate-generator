d60bec833bbd0ca3dd20ddeddc3644757b6f30ee
Elanor Krause <elanor.krause@diva-e.com>
2024-05-08 17:02:05 +0200

    initialized project

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# eds-boilerplate-generator
+# diva-e EDS boilerplate generator
+
+This package allows you to clone the **diva-e EDS boilerplate repository** and quickly generate **boilerplate components**.
+
+## Clone repository
+
+Use the command below to clone the diva-e EDS boilerplate repository into a folder of your choice:
+
+```sh
+npx eds-generator clone <folder-name>
+```
+
+## Generate components
+
+Use the command below to start the component generator and follow the displayed instructions:
+
+```sh
+npx eds-generator new
+```
---
+++ componentGenerator/index.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+import path from 'node:path';
+import chalk from 'chalk';
+import figlet from 'figlet';
+import inquirer from 'inquirer';
+import generateFiles from './generateFiles.js';
+
+function printIntroText() {
+  console.log(
+    chalk.yellow(
+      figlet.textSync('Generate Component', {
+        horizontalLayout: 'full',
+      })
+    )
+  );
+
+  console.log(
+    'You can enter a ' + chalk.bold('name') + " for a new component. e.g. '" + chalk.green('Related Topics') + "'."
+  );
+  console.log('All necessary files whit some boilerplate code and their imports will then be generated.');
+  console.log('e.g.');
+  console.log('typescript file: ', chalk.cyan(`related-topics.ts`));
+  console.log('scss file: ', chalk.cyan(`related-topics.scss`));
+  console.log('\nYou can press ', chalk.bgWhite.black(' ctrl + c '), ' any time to cancel the process.\n\n');
+}
+
+// returns user config with prompts and file generators will fallback to module config.js
+async function getConfig(configPath) {
+  let config;
+
+  try {
+    config = await import(path.resolve(configPath));
+  } catch (error) {
+    // user did not provide a custom config, use the default one
+    // there was some error in the user's config file
+    if (!error.message.startsWith('Cannot find module')) throw error;
+    config = await import('./config.js');
+  }
+
+  return config;
+}
+
+/**
+ * asks the user information regarding the necessary files for the new component
+ *
+ * @param {GenerateComponent.CliOptions} cliOptions - enquiry options
+ * @returns {Promise<Object>} - info which user has filled in.
+ */
+async function enquiry(prompts, cliOptions = {}) {
+  const answers = await inquirer.prompt(prompts, cliOptions);
+  return answers;
+}
+
+export async function createNewComponent(cliOptions) {
+  printIntroText();
+
+  if (cliOptions.name) console.log(chalk.green('?'), 'Component Name?', chalk.cyan(cliOptions.name));
+
+  const config = await getConfig(cliOptions.config);
+  const prompts = config.default.map((item) => item.prompt).filter((prompt) => prompt !== undefined);
+  const options = await enquiry(prompts, { ...cliOptions });
+
+  if (cliOptions.verbose) console.log('Filled in data: ', options);
+
+  await generateFiles(options, config.default, true);
+
+  process.exit = 0;
+}
