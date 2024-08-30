12fbb5d828c7f775021372430c8a5c3de8d9668a
samuelecalamita <samuelecalamita@gmail.com>
2024-08-30 20:43:47 +0200

    added generate project

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 import { select, input } from '@inquirer/prompts';
 import chalk from 'chalk';
 import figlet from 'figlet';
+import fs from "node:fs";
+import path from "node:path";
 
 const XWALK_TYPE = 'x-walk';
 const DOC_BASED_TYPE = 'doc-based';
@@ -15,6 +17,27 @@ console.log(
     })
   )
 );
+
+const generateProject = async ({projectName, projectType, contentSourceURL}) => {
+  const projectPath = path.join(process.cwd(), projectName);
+  if (fs.existsSync(projectPath)) {
+    console.log(chalk.red(`The project ${projectName} already exists in the current directory`));
+    return;
+  }
+
+  fs.mkdirSync(projectPath);
+  
+  fs.cpSync("./src/shared/", projectPath, { recursive: true });
+
+  if (projectType === XWALK_TYPE) {
+    fs.cpSync("./src/x-walk/", projectPath, { recursive: true }); // force is needed to overwrite the existing files
+
+  }
+
+  if (projectType === DOC_BASED_TYPE) {
+    fs.cpSync("./src/doc-based/", projectPath, { recursive: true }); // force is needed to overwrite the existing files
+  }
+};
 
 const projectType = await select({ 
   message: 'Which project template would you like to generate?', 
@@ -36,7 +59,10 @@ if (projectType === XWALK_TYPE) {
     { 
       message: 'Enter the Author AEMaaCS URL https://author-pXXXXXX-XXXXX.adobeaemcloud.com/ or just press enter to skip',
     });
+
   console.log('Generating X-Walk project ' + projectName);
+
+  generateProject({projectName, projectType, contentSourceURL});
 }
 
 if (projectType === DOC_BASED_TYPE) {
@@ -47,4 +73,6 @@ if (projectType === DOC_BASED_TYPE) {
     });
 
   console.log('Generating Doc-Based project' + projectName);
+
+  generateProject({projectName, projectType, contentSourceURL});
 }
