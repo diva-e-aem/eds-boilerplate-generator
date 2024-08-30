9b5474977ed88c6ac71ef3278d2501be461a2b70
samuelecalamita <samuelecalamita@gmail.com>
2024-08-30 12:56:55 +0200

    remove sass folder and unused css files

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -15,14 +15,14 @@ export interface Config {
 
 export const config: Config = {
   mainTsPath: './src/main.ts',
-  mainScssPath: './src/styles/sass/main.scss',
+  mainScssPath: './src/styles/main.scss',
   iconsDirPath: './public/icons',
   iconsTypesPath: './src/types/icons.types.ts',
-  fontsScssPath: './src/styles/sass/fonts.scss',
+  fontsScssPath: './src/styles/fonts.scss',
   fontsCssPath: './dist/fonts/fonts.css',
-  lazyStylesScssPath: './src/styles/sass/lazy-styles.scss',
+  lazyStylesScssPath: './src/styles/lazy-styles.scss',
   lazyStylesCssPath: './dist/lazyStyles/lazyStyles.css',
-  sidekickLibraryStylesScssPath: './src/styles/sass/sidekick-library-styles.scss',
+  sidekickLibraryStylesScssPath: './src/styles/sidekick-library-styles.scss',
   sidekickLibraryStylesCssPath: './dist/sidekickLibraryStyles/sidekickLibraryStyles.css',
   editorTsPath: './src/app/editor/editor-support.ts',
   lcpBlocks: [],
